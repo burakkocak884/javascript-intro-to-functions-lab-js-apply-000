ef660c0722d49c31ae53a4928df83795ecc5e9ef
BURAK KOCAK <burakkocak884@hotmail.com>
2018-11-20 18:15:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
 if(sayHiToGrandma = "hello"){
-   return ("I can't hear you!")
+   return ("I can't hear you!");
 }else if(sayHiToGrandma = "HELLO"){
-   return ("YES INDEED!")
+   return ("YES INDEED!");
 }else if (sayHiToGrandma = "I love you, Grandma."){
-return ("I Love You, too.")
+return ("I Love You, too.");
 }
 }
